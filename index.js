4305ac6170a4f1d987523b423e56dce3a2da07f9
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 15:28:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 theBeatlesPlay(musicians, instruments){
   var arr = {};
 
-  for(var i = 0; musicians.length <= 0; i++){
+  for(var i = 0; musicians.length - 1 <= 0; i++){
     var str = musician[i] + " plays " + instruments[i];
     arr.push(str);
   }
